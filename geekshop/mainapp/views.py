dd2bf7f38297ac28093de088f197c76fc8de0921
Dmitry Takmakov <dj-dat@yandex.ru>
2020-09-23 22:33:25 +0500

    Homework for lesson 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
+import json
+
+links_menu = [
+    {'href': 'products_all', 'name': 'все'},
+    {'href': 'products_home', 'name': 'дом'},
+    {'href': 'products_office', 'name': 'офис'},
+    {'href': 'products_modern', 'name': 'модерн'},
+    {'href': 'products_classic', 'name': 'классика'},
+]
+
+with open('/home/dimadat/PycharmProjects/django_basics/geekshop/mainapp/templates/mainapp/inc/inc_products_categories'
+          '.json', 'r') as categories:
+    products_categories = json.load(categories)
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'главная',
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    content = {
+        'title': "продукты",
+        'links_menu': links_menu,
+        'products_categories': products_categories,
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'контакты',
+    }
+    return render(request, 'mainapp/contact.html', content)
