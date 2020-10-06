c975563442f45bd20cbeef62b16d46fdfb7c6411
Dmitry Takmakov <dj-dat@yandex.ru>
2020-10-06 05:35:46 +0500

    small fix

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-import json
+from django.shortcuts import render, get_object_or_404
 
-from mainapp.models import ProductCategory
+from basketapp.models import Basket
+from mainapp.models import ProductCategory, Product
 
 
 def main(request):
@@ -13,9 +13,30 @@ def main(request):
 
 def products(request, pk=None):
     links_menu = ProductCategory.objects.all()
+
+    basket = Basket.objects.filter(user=request.user)
+
+    if pk is not None:
+        if pk == 0:
+            products_list = Product.objects.all()
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products_list = Product.objects.filter(category=category)
+
+        content = {
+            'title': "продукты",
+            'links_menu': links_menu,
+            'category': category,
+            'products': products_list,
+            'basket': basket,
+        }
+        return render(request, 'mainapp/products_list.html', content)
+
     content = {
         'title': "продукты",
         'links_menu': links_menu,
+        'basket': basket,
     }
     return render(request, 'mainapp/products.html', content)
 
