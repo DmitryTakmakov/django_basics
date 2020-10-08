8cdcece5041b8be09e290c184076893528b64048
Dmitry Takmakov <dj-dat@yandex.ru>
2020-10-08 05:16:27 +0500

    lesson 6

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -9,3 +9,19 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     added_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def product_price(self):
+        return self.product.price * self.quantity
+
+    @property
+    def total_quantity(self):
+        _items = Basket.objects.filter(user=self.user)
+        _total_quantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _total_quantity
+
+    @property
+    def total_cost(self):
+        _items = Basket.objects.filter(user=self.user)
+        _total_cost = sum(list(map(lambda x: x.product_price, _items)))
+        return _total_cost
